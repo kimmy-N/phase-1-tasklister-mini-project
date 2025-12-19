5bd209bb88ff036f26f8b15cef8eca405b365e98
kimmy-N <ayiakinashipai@gmail.com>
2025-12-19 16:37:12 +0300

    Implement task creation from form submission
    Added functionality to create and display tasks from a form submission.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskText = event.target.elements["new-task-description"].value;
+    const data = { task: taskText };
+    if (data.task.trim() !== "") {
+      buildToDo(data.task);
+      event.target.elements["new-task-description"].value = "";
+    }
+  });
+
+  function buildToDo(task) {
+    const taskList = document.getElementById("tasks");
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    taskList.appendChild(newTask);
+  }
 });
